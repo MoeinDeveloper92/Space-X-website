16272ceb029d01872494e1515999acd3c17d0ee7
moein <moeinsamani1992@gmail.com>
2023-11-20 17:38:14 +0100

    another changes

--- js/script.js
+++ js/script.js
@@ -1,73 +1,14 @@
-const btn = document.querySelector('#menu-btn');
-const overlay = document.getElementById('overlay');
-const mobileMenue = document.getElementById('mobile-menu');
-btn.addEventListener("click", navToggle)
-const menu = document.getElementById("mobile-menu")
-const counters = document.querySelectorAll(".counter")
+const btn = document.getElementById("menu-btn")
+const overlay = document.getElementById("overlay")
+const mobileMenu = document.getElementById("mobile-menu")
 
-let scrollStarted = false
+
+btn.addEventListener("click", navToggle)
+
 
 function navToggle() {
-    btn.classList.toggle('open')
-    overlay.classList.toggle('overlay-show')
-    document.body.classList.toggle('stop-scrolling')
-    menu.classList.toggle('show-menu')
-    mobileMenue.classList.toggle('active')
-
+    btn.classList.toggle("open")
+    overlay.classList.toggle("overlay-show")
+    document.body.classList.toggle("stop-scrolling")
+    mobileMenu.classList.toggle("mobile-show")
 }
-
-document.addEventListener("scroll", scrollPage)
-
-
-function scrollPage() {
-    const scrollPos = window.scrollY
-    if (scrollPos > 100 && !scrollStarted) {
-        countUp()
-        scrollStarted = true
-    } else if (scrollPos < 100 && scrollStarted) {
-        reset()
-        scrollStarted = false
-    }
-}
-
-
-function countUp() {
-    counters.forEach((counter) => {
-        counter.innerText = "0"
-
-        const updateCounter = () => {
-
-            //getting the counter's data-target
-            const target = +counter.getAttribute("data-target")
-
-            //Get the current value of the counter
-            const c = +counter.innerText
-
-            //create an increment
-            const increment = target / 100
-
-            // if counter is less thatn the target
-            if (c < target) {
-                //round up the counter
-                counter.innerText = `${Math.ceil(c + increment)}`
-
-                setTimeout(updateCounter, 75)
-            } else {
-                counter.innerText = target
-            }
-        }
-
-        updateCounter()
-    })
-}
-
-
-function reset() {
-    counters.forEach((counter) => counter.innerHTML = "0")
-}
-
-countUp()
-
-
-
-
